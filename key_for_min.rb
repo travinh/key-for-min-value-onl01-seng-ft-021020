d231e3d00108b217d8e49326f793fbdbfbdb1f17
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 05:46:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = name_hash.values[0]
+  min_value = name_hash.keys[0]
   min_index = 0
   current_index = 0
   if !name_hash.empty?
