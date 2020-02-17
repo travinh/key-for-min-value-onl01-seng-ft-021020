303c34ab7ead3f2c699d59f17543fdf09bbb3f15
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 05:55:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  min_value = name_hash.value[0]
-  min_key = name_hash.key[0]
-  min_index = 0
-  current_index = 0
-  if !name_hash.empty?
-    name_hash.each do |name,number|
-      if number < min_value
-        min_value = number
-        min_index = current_index
-      end
-      current_index+= 1
-    end
-    name_hash.keys[min_index]
-  else
-    return nil
-  end
-end
+# def key_for_min_value(name_hash)
+#   min_value = name_hash.value[0]
+#   min_key = name_hash.key[0]
+#   min_index = 0
+#   current_index = 0
+#   if !name_hash.empty?
+#     name_hash.each do |name,number|
+#       if number < min_value
+#         min_value = number
+#         min_index = current_index
+#       end
+#       current_index+= 1
+#     end
+#     name_hash.keys[min_index]
+#   else
+#     return nil
+#   end
+# end
