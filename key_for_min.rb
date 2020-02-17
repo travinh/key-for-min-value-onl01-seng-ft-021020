86d0ec30eab5a788411544b18b37bd16b99b2ee9
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 05:35:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,13 @@ def key_for_min_value(name_hash)
   min_index = 0
   current_index = 0
   if !name_hash.empty?
-    
-  name_hash.each do |key,value|
-    if value < min_value
-      min_value = name_hash[value]
-      min_index = current_index
+    name_hash.each do |key,value|
+      if value < min_value
+        min_value = name_hash[value]
+        min_index = current_index
+      end
+      current_index+=1
     end
-    current_index+=1
-  end
+  else
+    return nil
 end
