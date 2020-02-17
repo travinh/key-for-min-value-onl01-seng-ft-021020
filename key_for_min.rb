bc650924577c44a5fd7e3f7338bcdf3c3052fe54
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 05:37:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = name_hash.value[0]
+  min_value = name_hash.values[0]
   min_index = 0
   current_index = 0
   if !name_hash.empty?
     name_hash.each do |key,value|
       if value < min_value
-        min_value = name_hash[value]
+        min_value = name_hashs[value]
         min_index = current_index
       end
       current_index+=1
